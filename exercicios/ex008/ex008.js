07d2950469ca652babfd4604e041561fa12cecec
Vinicius Pitthan <viniciuspitthan74@gmail.com>
2024-10-08 10:52:36 -0300

    ex008

---
+++ exercicios/ex008/ex008.js
@@ -0,0 +1,6 @@
+var vel = 59
+console.log(`A velocidade do seu carro é ${vel}KM/h`)
+if(vel > 60){ //condição simples
+    console.log(`Você ultrapassou a velocidade permitida. MULTADO!`)
+}
+console.log(`Dirija sempre usando cinto de segurança!`)
